8da4ff7ddbf3bcaf028f94a9ab4fa6c4ff929886
nlewis84 <nathan@differential.com>
2022-01-16 12:16:49 -0600

    chore: new fn files

---
+++ src/helpers/musicSetup.js
@@ -0,0 +1,21 @@
+
+const MUSIC_MEMORY_PIECES = [
+  {selection: 'Minuet in G', composer: 'Beethoven', youtube: 'zSXRJwspGU0'},
+  {majorWork: 'The Creation', selection: 'The Heavens Are Telling', composer: 'Haydn', youtube: 'OwqqfbinUDY'},
+  {majorWork: 'Cello Suite No. 1 in G Major', selection: 'Prelude', composer: 'Bach', youtube: '1prweT95Mo0'},
+  {majorWork: 'Symphonie Fantastique', selection: 'March to the Scaffold', composer: 'Berlioz', youtube: 'QwCuFaq2L3U'},
+  {selection: 'Short Ride in a Fast Machine', composer: 'Adams', youtube: 'rx9JUyFqTJI'},
+  {majorWork: 'Nabucco', selection: 'Va, pensiero', composer: 'Verdi', youtube: 'XObwULrJ5xg'},
+  {majorWork: "Young Person's Guide to the Orchestra", selection: 'Fugue', composer: 'Britten', youtube: 'UuRdm9x6LaI'},
+  {selection: 'Hungarian Dance No. 5', composer: 'Brahms', youtube: '-HNmVe0Mmek'},
+  {majorWork: 'Three Gymnopedies', selection: 'No. 1', composer: 'Satie', youtube: 'S-Xm7s9eGxU'},
+  {majorWork: 'West Side Story', selection: 'Dance at the Gym (Mambo)', composer: 'Bernstein', youtube: 'Pqn-lO2QCGc'},
+  {majorWork: 'Viola Concerto in G Major', selection: 'Movement 2', composer: 'Telemann', youtube: 'iTbwMis5Xn4'},
+  {majorWork: 'Symphony No. 40', selection: 'Movement 1', composer: 'Mozart', youtube: 'BJPmYURJk4c'},
+  {selection: "It Don't Mean a Thing If It Ain't Got That Swing", composer: 'Ellington', youtube: 'myRc-3oF1d0'},
+  {selection: 'Circus Polka (For a Young Elephant)', composer: 'Stravinksy', youtube: 'kkL9LS2ZfUE'},
+  {selection: 'Medalist Fanfare', composer: 'Giroux', youtube: 'tEjso5bweQY'},
+  {selection: 'Fanfare for the Common Man', composer: 'Copland', youtube: '9dljTJ9qJ4U'}
+]
+
+export default MUSIC_MEMORY_PIECES;
