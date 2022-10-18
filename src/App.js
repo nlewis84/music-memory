a1f37aae863bc5a020b0be2eb5be24903dafaefc
nlewis84 <nathan@differential.com>
2022-10-17 21:23:31 -0500

    fix: working tablet and mobile version

--- src/App.js
+++ src/App.js
@@ -52,47 +52,6 @@ class App extends React.Component {
   render() {
     const { DataisLoaded, item } = this.state;
 
-    if (isMobile) {
-      return (
-        <div className="App">
-          <Container
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100vh',
-            }}
-          >
-            <Grid
-              container
-              spacing={2}
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Grid item xs={12}>
-                <Typography
-                  variant="h4"
-                  component="div"
-                  sx={{ flexGrow: 1, textAlign: 'center' }}
-                >
-                  Due to the limitations of auto play on phones and tablets,
-                  this site is designed for use on desktops, laptops, or
-                  chromebooks. Basically, Apple and Google have decided to
-                  protect you and your data limits from spammy videos eating up
-                  your data caps.
-                </Typography>
-              </Grid>
-            </Grid>
-          </Container>
-        </div>
-      );
-    }
-
     if (!DataisLoaded || !currentPiece?.youtube)
       return (
         <>
@@ -257,7 +216,342 @@ class App extends React.Component {
           </Container>
         </>
       );
-    console.log(item[0].url);
+
+    if (isMobile) {
+      return (
+        <>
+          <Paper
+            id="overlay"
+            className="overlay"
+            sx={{ display: 'none', backgroundColor: '#000000ee' }}
+            component={Stack}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Box
+              component={Stack}
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              spacing={5}
+            >
+              <img className="image" src={item[0].url} alt="cat gif" />
+              <Typography
+                variant="h4"
+                sx={{
+                  fontSize: 'h4.fontSize',
+                  fontWeight: 'bold',
+                  textAlign: 'center',
+                }}
+                id="reply"
+              >
+                placeholder
+              </Typography>
+            </Box>
+          </Paper>
+          <Container sx={{ p: 5 }}>
+            <Typography
+              variant="h2"
+              sx={{ fontWeight: 'bold', textAlign: 'center' }}
+            >
+              Texas UIL 3rd-6th Grade
+            </Typography>
+            <Typography variant="h3" sx={{ textAlign: 'center' }}>
+              Music Memory Game
+            </Typography>
+            <div
+              style={{
+                display: 'grid',
+                placeItems: 'top',
+                width: '300px',
+                height: '200px',
+                margin: '0 auto',
+              }}
+            >
+              <div
+                style={{
+                  gridArea: 'inner-div',
+                  zIndex: 1,
+                  background: '#E1E1E1',
+                  height: '50px',
+                  width: '300px',
+                }}
+              />
+              <div style={{ gridArea: 'inner-div', zIndex: 0 }}>
+                {StyledYouTube(currentPiece.youtube, true)}
+              </div>
+            </div>
+
+            <Container>
+              <Grid
+                container
+                sx={{ gap: 2, mt: 5 }}
+                spacing={2}
+                alignItems="center"
+                justifyContent="center"
+              >
+                {MUSIC_MEMORY_PIECES.map((item, i) => {
+                  return (
+                    <Card
+                      key={i}
+                      sx={{
+                        backgroundColor: 'primary.dark',
+                        borderRadius: 2,
+                        height: 180,
+                        width: 180,
+                        ':hover': {
+                          boxShadow: 10,
+                        },
+                        ':active': { boxShadow: 0 },
+                      }}
+                      variant="outlined"
+                      style={{ textDecoration: 'none' }}
+                      onClick={(e) => correctPiece(e, item)}
+                    >
+                      <CardContent>
+                        {item.majorWork ? (
+                          <>
+                            <Typography
+                              variant="h6"
+                              sx={{
+                                color: 'text.tertiary',
+                                fontWeight: 'bold',
+                                textAlign: 'center',
+                              }}
+                            >
+                              {item.majorWork}
+                            </Typography>
+                            <Typography
+                              variant="body1"
+                              sx={{
+                                color: 'text.primary',
+                                fontWeight: 'regular',
+                                textAlign: 'center',
+                              }}
+                            >
+                              {item.selection}
+                            </Typography>
+                          </>
+                        ) : (
+                          <Typography
+                            variant="body1"
+                            sx={{
+                              color: 'text.primary',
+                              fontWeight: 'regular',
+                              textAlign: 'center',
+                            }}
+                          >
+                            {item.selection}
+                          </Typography>
+                        )}
+                        <Typography
+                          variant="body1"
+                          sx={{ color: 'text.disabled', textAlign: 'center' }}
+                        >
+                          {item.composer}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  );
+                })}
+              </Grid>
+            </Container>
+          </Container>
+          <Stack
+            sx={{
+              position: 'fixed',
+              width: '100px',
+              bottom: 0,
+              left: 0,
+              m: 2,
+              zIndex: 1,
+            }}
+            direction="column"
+            spacing={2}
+          >
+            <Grid
+              container
+              sx={{ gap: 2, mt: 5 }}
+              spacing={2}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Grid item>
+                <Tooltip title="About me">
+                  <Fab
+                    color="primary"
+                    aria-label="about"
+                    onClick={() => window.open('https://nathanlewis.dev/about')}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      fill="#ffffff"
+                      viewBox="0 0 256 256"
+                    >
+                      <rect width="256" height="256" fill="none"></rect>
+                      <path
+                        d="M158.6,93.3a20,20,0,0,1,34.7-20l20,34.7A80,80,0,0,1,74.7,188l-42-72.8a20,20,0,0,1,34.7-20l-16-27.7A20,20,0,0,1,86,47.5l8,13.9a20,20,0,0,1,34.6-20Z"
+                        opacity="0.2"
+                      ></path>
+                      <path
+                        d="M94,61.4a20,20,0,0,1,34.6-20l30,51.9"
+                        fill="none"
+                        stroke="#ffffff"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></path>
+                      <path
+                        d="M67.4,95.2l-16-27.7A20,20,0,0,1,86,47.5l34,58.9"
+                        fill="none"
+                        stroke="#ffffff"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></path>
+                      <path
+                        d="M154,165.3a39.9,39.9,0,0,1,14.6-54.6l-10-17.4a20,20,0,0,1,34.7-20l20,34.7A80,80,0,0,1,74.7,188l-42-72.8a20,20,0,0,1,34.7-20l22,38.1"
+                        fill="none"
+                        stroke="#ffffff"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></path>
+                      <path
+                        d="M81.1,240A110.3,110.3,0,0,1,48,204"
+                        fill="none"
+                        stroke="#ffffff"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></path>
+                      <path
+                        d="M176,31a51.7,51.7,0,0,1,45,26"
+                        fill="none"
+                        stroke="#ffffff"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></path>
+                    </svg>
+                  </Fab>
+                </Tooltip>
+              </Grid>
+            </Grid>
+          </Stack>
+          <Stack
+            sx={{
+              position: 'fixed',
+              width: '100px',
+              bottom: 0,
+              right: 0,
+              m: 2,
+              zIndex: 1,
+            }}
+            direction="column"
+            spacing={2}
+          >
+            <Grid
+              container
+              sx={{ gap: 2, mt: 5 }}
+              spacing={2}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Grid item>
+                <Tooltip title="Buy me a coffee!">
+                  <Fab
+                    color="primary"
+                    aria-label="add"
+                    onClick={() =>
+                      window.open('https://account.venmo.com/u/Nathan-Lewis-35')
+                    }
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      fill="#000000"
+                      viewBox="0 0 256 256"
+                    >
+                      <rect width="48" height="48" fill="none"></rect>
+                      <path
+                        d="M83.3,216A88,88,0,0,1,32,136V88H208v48a88,88,0,0,1-51.3,80Z"
+                        opacity="0.2"
+                      ></path>
+                      <line
+                        x1="88"
+                        y1="24"
+                        x2="88"
+                        y2="56"
+                        fill="none"
+                        stroke="#FFFFFF"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></line>
+                      <line
+                        x1="120"
+                        y1="24"
+                        x2="120"
+                        y2="56"
+                        fill="none"
+                        stroke="#FFFFFF"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></line>
+                      <line
+                        x1="152"
+                        y1="24"
+                        x2="152"
+                        y2="56"
+                        fill="none"
+                        stroke="#FFFFFF"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></line>
+                      <line
+                        x1="32"
+                        y1="216"
+                        x2="208"
+                        y2="216"
+                        fill="none"
+                        stroke="#FFFFFF"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></line>
+                      <path
+                        d="M83.3,216A88,88,0,0,1,32,136V88H208v48a88,88,0,0,1-51.3,80"
+                        fill="none"
+                        stroke="#FFFFFF"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></path>
+                      <path
+                        d="M208,88h0a32,32,0,0,1,32,32v8a32,32,0,0,1-32,32h-3.4"
+                        fill="none"
+                        stroke="#FFFFFF"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="16"
+                      ></path>
+                    </svg>
+                  </Fab>
+                </Tooltip>
+              </Grid>
+            </Grid>
+          </Stack>
+        </>
+      );
+    }
 
     return (
       <>
@@ -312,7 +606,7 @@ class App extends React.Component {
               mt: 2,
             }}
           >
-            Trouble? Click this button
+            No sound? Click this button
           </Typography>
           <Container
             sx={{
@@ -445,9 +739,93 @@ class App extends React.Component {
                 );
               })}
             </Grid>
-            {StyledYouTube(currentPiece.youtube)}
+            {StyledYouTube(currentPiece.youtube, false)}
           </Container>
         </Container>
+        <Stack
+          sx={{
+            position: 'fixed',
+            width: '100px',
+            bottom: 0,
+            left: 0,
+            m: 2,
+            zIndex: 1,
+          }}
+          direction="column"
+          spacing={2}
+        >
+          <Grid
+            container
+            sx={{ gap: 2, mt: 5 }}
+            spacing={2}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Grid item>
+              <Tooltip title="About this app">
+                <Fab
+                  color="primary"
+                  aria-label="about"
+                  onClick={() => window.open('www.nathanlewis.dev')}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="192"
+                    height="192"
+                    fill="#ffffff"
+                    viewBox="0 0 256 256"
+                  >
+                    <rect width="256" height="256" fill="none"></rect>
+                    <path
+                      d="M158.6,93.3a20,20,0,0,1,34.7-20l20,34.7A80,80,0,0,1,74.7,188l-42-72.8a20,20,0,0,1,34.7-20l-16-27.7A20,20,0,0,1,86,47.5l8,13.9a20,20,0,0,1,34.6-20Z"
+                      opacity="0.2"
+                    ></path>
+                    <path
+                      d="M94,61.4a20,20,0,0,1,34.6-20l30,51.9"
+                      fill="none"
+                      stroke="#ffffff"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="16"
+                    ></path>
+                    <path
+                      d="M67.4,95.2l-16-27.7A20,20,0,0,1,86,47.5l34,58.9"
+                      fill="none"
+                      stroke="#ffffff"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="16"
+                    ></path>
+                    <path
+                      d="M154,165.3a39.9,39.9,0,0,1,14.6-54.6l-10-17.4a20,20,0,0,1,34.7-20l20,34.7A80,80,0,0,1,74.7,188l-42-72.8a20,20,0,0,1,34.7-20l22,38.1"
+                      fill="none"
+                      stroke="#ffffff"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="16"
+                    ></path>
+                    <path
+                      d="M81.1,240A110.3,110.3,0,0,1,48,204"
+                      fill="none"
+                      stroke="#ffffff"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="16"
+                    ></path>
+                    <path
+                      d="M176,31a51.7,51.7,0,0,1,45,26"
+                      fill="none"
+                      stroke="#ffffff"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="16"
+                    ></path>
+                  </svg>
+                </Fab>
+              </Tooltip>
+            </Grid>
+          </Grid>
+        </Stack>
         <Stack
           sx={{
             position: 'fixed',
--- src/helpers/styledYouTube.js
+++ src/helpers/styledYouTube.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import YouTube from 'react-youtube';
 
-function StyledYouTube(id) {
+function StyledYouTube(id, mobile) {
   let array = [
-    0.07, 0.14, 0.21, 0.28, 0.35, 0.42, 0.49, 0.56, 0.63, 0.7, 0.77, 0.84, 0.91,
+    0.07, 0.14, 0.21, 0.28, 0.35, 0.42, 0.49, 0.56, 0.63, 0.7, 0.77, 0.82, 0.86,
   ];
   let randomTime = Math.floor(Math.random() * 13);
   let isUnMuted = true;
@@ -13,16 +13,25 @@ function StyledYouTube(id) {
 
     if (e.target.isMuted() && e.target.getPlayerState() === 1 && isUnMuted) {
       e.target.unMute();
-      // Sing, Sing, Sing, Vivaldi, and Barber of Seville are REALLY loud.....turn it down
-      id === 'HDsWyXIR1to' || id === 'JhLxK2xGqqo' || id === 'IVyRd9mlGyQ'? e.target.setVolume(50) : e.target.setVolume(75);
+      // Sing, Sing, Sing, Vivaldi, Entertainer, and Barber of Seville are REALLY loud.....turn it down
+      id === 'HDsWyXIR1to' ||
+      id === 'JhLxK2xGqqo' ||
+      id === 'g8syRhvSZdk' ||
+      id === 'IVyRd9mlGyQ'
+        ? e.target.setVolume(50)
+        : e.target.setVolume(75);
       e.target.seekTo(duration * array[randomTime]);
       e.target.playVideo(); // resume playback
       isUnMuted = false; // you want to run this once only!
+      // change the opts.height to 0 to hide the video
+      if (mobile) {
+        e.target.setSize(0, 0);
+      }
     }
   };
 
   const opts = {
-    height: '0',
+    height: mobile ? '100%' : '0%',
     width: '100%',
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
